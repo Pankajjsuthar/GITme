4c8cbf30a8e90bf3a2854993fd2fce84b784e9bf
Pankaj Suthar <pankajsuthar27302@gmail.com>
2022-07-25 16:13:33 +0530

    gitme first commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class GitHub {
+    constructor() {
+      this.client_id = 'd81364b5700907b33ac5';
+      this.client_secret = '58d6aabc93fa7f74be3bdc2f392deead71223efb';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
+    }
+  
+    async getUser(user) {
+      const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+  
+      const profile = await profileResponse.json();
+      const repos = await repoResponse.json();
+  
+      return {
+        profile,
+        repos
+      }
+    }
+  }
